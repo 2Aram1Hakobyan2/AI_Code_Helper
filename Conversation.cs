b49f9e710d5f48f4241cdc695ace65ac61910b42
Aram <130496758+2Aram1Hakobyan2@users.noreply.github.com>
2023-11-10 18:55:11 +0400

    Add files via upload

---
+++ Conversation.cs
@@ -0,0 +1,7 @@
+﻿public class Conversation
+{
+    public int Id { get; set; }
+    public string TheQuestion { get; set; } //The Question Asked by the User
+    public string ChatBotAnswer { get; set; } //The Anwer from the chat bot
+    public DateTime TimeTaken { get; set; } = DateTime.UtcNow;
+}
